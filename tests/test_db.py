584eb1efaa25b3132ca196d9a97a757dd835fd9c
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-02 18:26:44 +0000

    Add: test for sql_resd_query

---
+++ tests/test_db.py
@@ -0,0 +1,18 @@
+import unittest
+from scripts import db
+
+
+class TestdbModule(unittest.TestCase):
+    def setUp(self) -> None:
+        self.database_path = 'files/database.db'
+        self.conn = db.sql_connection(self.database_path)
+    def test_sql_read_query(self):
+        book_id = 3139992
+        rows = db.sql_read_query(self.conn, "BOOK_ID", book_id)
+        self.assertEqual(rows[0]['id'],4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
