35b0be5def6f31d191c525a20ae4d54bb12c4369
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-05 19:17:38 +0000

    Add: settings.py

---
+++ scripts/settings.py
@@ -0,0 +1,9 @@
+# Scrape
+page_n = 25
+page = 1
+keyword = "manning"
+url = f"https://libgen.is/search.php?&req={keyword}&phrase=1&view=detailed&res={page_n}&column=def&sort=year&sortmode=DESC&page={page}"
+
+# Database
+database_path = 'files/database.db'
+
